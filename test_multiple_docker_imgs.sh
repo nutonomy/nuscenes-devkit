89f52a8b1be8549e10f189cdc4788057e347e91b
Christopher Li <christopher.li@nutonomy.com>
2019-03-18 12:10:57 -0700

    * Moved tests across multiple Docker images into a single bash script.

---
+++ test_multiple_docker_imgs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+bash test_mini_split.sh Dockerfile_3.5
+exit_code_3_5="$?"
+
+bash test_mini_split.sh Dockerfile_3.6
+exit_code_3_6="$?"
+
+bash test_mini_split.sh Dockerfile_3.7
+exit_code_3_7="$?"
+
+if [ ${exit_code_3_5} -neq 0 ]; then
+    echo "Failed with Python 3.5 environment"
+    exit 1
+elif [ ${exit_code_3_6} -neq 0 ]; then
+    echo "Failed with Python 3.6 environment"
+    exit 1
+elif [ ${exit_code_3_7} -neq 0 ]; then
+    echo "Failed with Python 3.5 environment"
+    exit 1
+else
+    echo "Passed all unit tests across Docker images."
+fi
