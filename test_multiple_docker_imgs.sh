655001039e256cddfb29b10556ed0124ac83ab1e
Christopher Li <christopher.li@nutonomy.com>
2019-03-18 13:55:49 -0700

    * Removed Dockerfile with Python 3.5 from continuous integration.

--- test_multiple_docker_imgs.sh
+++ test_multiple_docker_imgs.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-bash test_mini_split.sh Dockerfile_3.5
-exit_code_3_5="$?"
-
 bash test_mini_split.sh Dockerfile_3.6
 exit_code_3_6="$?"
 
 bash test_mini_split.sh Dockerfile_3.7
 exit_code_3_7="$?"
 
-if [ ${exit_code_3_5} -ne 0 ]; then
-    echo "Failed with Python 3.5 environment"
-    exit 1
-elif [ ${exit_code_3_6} -ne 0 ]; then
+if [ ${exit_code_3_6} -ne 0 ]; then
     echo "Failed with Python 3.6 environment"
     exit 1
 elif [ ${exit_code_3_7} -ne 0 ]; then
