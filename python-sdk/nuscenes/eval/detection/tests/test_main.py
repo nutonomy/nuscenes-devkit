c34a65e41424fbbc8ddeba62bdff82cecca2656f
Oscar Beijbom <oscar@nutonomy.com>
2019-03-20 13:23:26 -0700

    updated test score

--- python-sdk/nuscenes/eval/detection/tests/test_main.py
+++ python-sdk/nuscenes/eval/detection/tests/test_main.py
@@ -114,8 +114,9 @@ class TestMain(unittest.TestCase):
         # 4. Score = 0.20478832626986893. Updated treatment of cones, barriers, and other algo tunings.
         # 5. Score = 0.2043569666105005. AP calculation area is changed from >=min_recall to >min_recall.
         # 6. Score = 0.20636954644294506. After bike-rack filtering.
-        # 7. Score - 0.20237925145690996. After TP reversion bug.
-        self.assertAlmostEqual(metrics.weighted_sum, 0.20237925145690996)
+        # 7. Score = 0.20237925145690996. After TP reversion bug.
+        # 8. Score = 0.24047129251302665. After bike racks bug.
+        self.assertAlmostEqual(metrics.weighted_sum, 0.24047129251302665)
 
 
 if __name__ == '__main__':
