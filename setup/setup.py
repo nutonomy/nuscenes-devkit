08dfd368fabfb6fbd8c76e8b500b20e447b2319b
alex-nutonomy <alex@nutonomy.com>
2019-03-21 16:27:34 -0700

    next pip package

--- setup/setup.py
+++ setup/setup.py
@@ -28,7 +28,7 @@ packages = [d.replace('/', '.').replace('{}.'.format(rootdir), '') for d in get_
 
 setuptools.setup(
     name='nuscenes-devkit',
-    version='0.3.7',
+    version='0.3.8',
     author="Holger Caesar, Qiang Xu, Oscar Beijbom et al.",
     author_email="nuscenes@nutonomy.com",
     description="The official devkit of the nuScenes dataset",
