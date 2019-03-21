55feace8fff18345c1f347fe8d1da83fc6063fc7
Holger Caesar <holger@nutonomy.com>
2019-03-21 14:22:10 +0800

    Removed v1.0 which was used for setup purposes

--- python-sdk/nuscenes/nuscenes.py
+++ python-sdk/nuscenes/nuscenes.py
@@ -45,7 +45,7 @@ class NuScenes:
         :param verbose: Whether to print status messages during load.
         :param map_resolution: Resolution of maps (meters).
         """
-        if version not in ['v1.0', 'v1.0-trainval', 'v1.0-test', 'v1.0-mini']:  # TODO: v1.0 is only for testing, remove it
+        if version not in ['v1.0-trainval', 'v1.0-test', 'v1.0-mini']:
             raise ValueError('Invalid DB version: {}'.format(version))
 
         self.version = version
