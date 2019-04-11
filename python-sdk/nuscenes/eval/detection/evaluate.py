6c16f224ab3d3c2100947248201b513eff41085a
Holger Caesar <holger@nutonomy.com>
2019-04-11 13:58:03 +0800

    Fixed display bug

--- python-sdk/nuscenes/eval/detection/evaluate.py
+++ python-sdk/nuscenes/eval/detection/evaluate.py
@@ -153,10 +153,10 @@ class NuScenesEval:
         # -----------------------------------
         # Step 3: Print high-level metrics.
         # -----------------------------------
-        print('mean_ap: %.2f' % metrics_summary['mean_ap'] * 100)
+        print('mean_ap: %.2f' % (metrics_summary['mean_ap'] * 100))
         for tp_name, tp_val in metrics_summary['tp_errors'].items():
             print('%s: %.4f' % (tp_name, tp_val))
-        print('nd_score: %.2f' % metrics_summary['nd_score'] * 100)
+        print('nd_score: %.2f' % (metrics_summary['nd_score'] * 100))
         print('eval_time: %.1fs' % metrics_summary['eval_time'])
 
         return metrics, metric_data_list
