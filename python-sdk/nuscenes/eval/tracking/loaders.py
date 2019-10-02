22c998e30ecae8f04c642d77ba9d58de6fb92cf8
Holger Caesar <holger@nutonomy.com>
2019-10-02 20:28:31 +0800

    Reworked create_tracks to guarantee every timestamp is present

--- python-sdk/nuscenes/eval/tracking/loaders.py
+++ python-sdk/nuscenes/eval/tracking/loaders.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 
+import numpy as np
+
 from nuscenes.eval.common.data_classes import EvalBoxes
 from nuscenes.eval.tracking.data_classes import TrackingBox
 from nuscenes.nuscenes import NuScenes
@@ -7,34 +9,45 @@ from nuscenes.nuscenes import NuScenes
 
 def create_tracks(all_boxes: EvalBoxes, nusc: NuScenes) -> Dict[str, Dict[int, List[TrackingBox]]]:
     """
-    Returns all tracks for all scenes. This can be applied either to GT or predictions.
+    Returns all tracks for all scenes. Samples within a track are sorted in chronological order.
+    This can be applied either to GT or predictions.
     :param all_boxes:
     :param nusc: The NuScenes instance to load the sample information from.
     :return: The tracks.
     """
-    # Group annotations wrt scene and track_id.
+    # Init all scenes and timestamps to guarantee completeness.
     tracks = {}
-
-    for sample_token in all_boxes.sample_tokens:
-
+    scene_tokens = [nusc.get('sample', st)['scene_token'] for st in all_boxes.sample_tokens]
+    for scene_token in scene_tokens:
         # Init scene.
-        sample_record = nusc.get('sample', sample_token)
-        scene_token = sample_record['scene_token']
         if scene_token not in tracks:
             tracks[scene_token] = {}
 
+        # Init all timestamps in this scene.
+        scene = nusc.get('scene', scene_token)
+        cur_sample_token = scene['first_sample_token']
+        while True:
+            # Initialize array for current timestamp.
+            cur_sample = nusc.get('sample', cur_sample_token)
+            tracks[scene_token][cur_sample['timestamp']] = []
+
+            # Abort after the last sample.
+            if cur_sample_token == scene['last_sample_token']:
+                break
+
+            # Move to next.
+            cur_sample_token = cur_sample['next']
+
+    # Group annotations wrt scene and timestamp.
+    for sample_token in all_boxes.sample_tokens:
+        sample_record = nusc.get('sample', sample_token)
+        scene_token = sample_record['scene_token']
         boxes: List[TrackingBox] = all_boxes.boxes[sample_token]
         for box in boxes:
             # Augment the boxes with timestamp. We will use timestamps to sort boxes in time later.
             box.timestamp = sample_record['timestamp']
 
-            # Add box to tracks.
-            if box.timestamp not in tracks[scene_token].keys():
-                tracks[scene_token][box.timestamp] = []
+            # Add box to tracks. The timestamps have been initialized above.
             tracks[scene_token][box.timestamp].append(box)
-
-    # Make sure the tracks are sorted in time.
-    for scene_token, scene in tracks.items():
-        tracks[scene_token] = dict(sorted(tracks[scene_token].items(), key=lambda kv: kv[0]))
 
     return tracks
