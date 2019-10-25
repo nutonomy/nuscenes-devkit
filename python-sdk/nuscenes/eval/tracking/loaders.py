dea59843fb5c9645b7cc2e1b8d5fd269890c46ea
Caglayan (Cha) Dicle <caglayan.dicle@nutonomy.com>
2019-10-25 15:17:11 -0400

    Implement track interpolation

--- python-sdk/nuscenes/eval/tracking/loaders.py
+++ python-sdk/nuscenes/eval/tracking/loaders.py
@@ -1,9 +1,63 @@
+from bisect import bisect
 from typing import List, Dict
 
+import numpy as np
 from nuscenes.eval.common.data_classes import EvalBoxes
 from nuscenes.utils.splits import create_splits_scenes
 from nuscenes.eval.tracking.data_classes import TrackingBox
 from nuscenes.nuscenes import NuScenes
+
+
+def interpolate_tracking_boxes(left_box: TrackingBox, right_box: TrackingBox, right_ratio: float) -> TrackingBox:
+    def interp_list(left, right, rratio):
+        return tuple((1.0-rratio) * np.array(left, dtype=float) + rratio * np.array(right, dtype=float))
+
+    def interp_float(left, right, rratio):
+        return (1.0 - rratio) * float(left) + rratio * float(right)
+
+    return TrackingBox(sample_token=right_box.sample_token,
+                       translation=interp_list(left_box.translation, right_box.translation, right_ratio),
+                       size=interp_list(left_box.size, right_box.size, right_ratio),
+                       rotation=interp_list(left_box.rotation, right_box.rotation, right_ratio),
+                       velocity=interp_list(left_box.velocity, right_box.velocity, right_ratio),
+                       ego_dist=interp_float(left_box.ego_dist, right_box.ego_dist, right_ratio),
+                       tracking_id=right_box.tracking_id,
+                       tracking_name=right_box.tracking_name)
+
+
+def interpolate_tracks(tracks_by_timestamp: Dict[int, List[TrackingBox]]) -> Dict[int, List[TrackingBox]]:
+    # NOTE 1: We are rediscovering an information that already exists in nuscenes.
+    # NOTE 2: This interpolation does not take into account visibility. It would interpolate through occlusion.
+
+    # Group tracks by id
+    tracks_by_id = {}
+    track_timestamps_by_id = {}
+    for timestamp, tracking_boxes in tracks_by_timestamp.items():
+        for tracking_box in tracking_boxes:
+            if tracking_box.tracking_id not in tracks_by_id.keys():
+                tracks_by_id[tracking_box.tracking_id] = []
+                track_timestamps_by_id[tracking_box.tracking_id] = []
+            tracks_by_id[tracking_box.tracking_id].append(tracking_box)
+            track_timestamps_by_id[tracking_box.tracking_id].append(timestamp)
+
+    # Interpolate missing timestamps for each track
+    timestamps = tracks_by_timestamp.keys()
+    for timestamp in timestamps:
+        for tracking_id, track in tracks_by_id.items():
+            if track_timestamps_by_id[tracking_id][0] <= timestamp <= track_timestamps_by_id[tracking_id][-1] and \
+                    timestamp not in track_timestamps_by_id[tracking_id]:
+                # Find the closest before and after entries
+                right_ind = bisect(track_timestamps_by_id[tracking_id], timestamp)
+                left_ind = right_ind - 1
+                right_timestamp = track_timestamps_by_id[tracking_id][right_ind]
+                left_timestamp = track_timestamps_by_id[tracking_id][left_ind]
+                right_tracking_box = tracks_by_id[tracking_id][right_ind]
+                left_tracking_box = tracks_by_id[tracking_id][left_ind]
+                right_ratio = float(right_timestamp - timestamp)/(right_timestamp - left_timestamp)
+                tracking_box = interpolate_tracking_boxes(left_tracking_box, right_tracking_box, right_ratio)
+                tracks_by_timestamp[timestamp].append(tracking_box)
+
+    return tracks_by_timestamp
 
 
 def create_tracks(all_boxes: EvalBoxes, nusc: NuScenes, eval_split: str) -> Dict[str, Dict[int, List[TrackingBox]]]:
@@ -56,5 +110,6 @@ def create_tracks(all_boxes: EvalBoxes, nusc: NuScenes, eval_split: str) -> Dict
     # This is always the case for GT, but may not be the case for predictions.
     for scene_token in tracks.keys():
         tracks[scene_token] = dict(sorted(tracks[scene_token].items(), key=lambda kv: kv[0]))
+        tracks[scene_token] = interpolate_tracks(tracks[scene_token])
 
     return tracks
