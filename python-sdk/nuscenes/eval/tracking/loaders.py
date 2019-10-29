cb6f035a86bc7c9c0064cc5523817d4a08fc167b
Oscar Beijbom <oscar@nutonomy.com>
2019-10-29 14:25:08 -0700

    used defaultdict to simplify code

--- python-sdk/nuscenes/eval/tracking/loaders.py
+++ python-sdk/nuscenes/eval/tracking/loaders.py
@@ -1,5 +1,6 @@
 from bisect import bisect
 from typing import List, Dict
+from collections import defaultdict
 
 import numpy as np
 from pyquaternion import Quaternion
@@ -57,13 +58,10 @@ def interpolate_tracks(tracks_by_timestamp: Dict[int, List[TrackingBox]]) -> Dic
     :return: The interpolated tracks.
     """
     # Group tracks by id.
-    tracks_by_id = {}
-    track_timestamps_by_id = {}
+    tracks_by_id = defaultdict(list)
+    track_timestamps_by_id = defaultdict(list)
     for timestamp, tracking_boxes in tracks_by_timestamp.items():
         for tracking_box in tracking_boxes:
-            if tracking_box.tracking_id not in tracks_by_id.keys():
-                tracks_by_id[tracking_box.tracking_id] = []
-                track_timestamps_by_id[tracking_box.tracking_id] = []
             tracks_by_id[tracking_box.tracking_id].append(tracking_box)
             track_timestamps_by_id[tracking_box.tracking_id].append(timestamp)
 
@@ -113,27 +111,8 @@ def create_tracks(all_boxes: EvalBoxes, nusc: NuScenes, eval_split: str, gt: boo
         if scene['name'] in splits[eval_split]:
             scene_tokens.add(scene_token)
 
-    # Init all scenes and timestamps to guarantee completeness.
-    tracks = {}
-    for scene_token in scene_tokens:
-        # Init scene.
-        if scene_token not in tracks:
-            tracks[scene_token] = {}
-
-        # Init all timestamps in this scene.
-        scene = nusc.get('scene', scene_token)
-        cur_sample_token = scene['first_sample_token']
-        while True:
-            # Initialize array for current timestamp.
-            cur_sample = nusc.get('sample', cur_sample_token)
-            tracks[scene_token][cur_sample['timestamp']] = []
-
-            # Abort after the last sample.
-            if cur_sample_token == scene['last_sample_token']:
-                break
-
-            # Move to next sample.
-            cur_sample_token = cur_sample['next']
+    # Tracks is stored as dict {scene_token: {timestamp: List[TrackingBox]}}.
+    tracks = defaultdict(lambda: defaultdict(list))
 
     # Group annotations wrt scene and timestamp.
     for sample_token in all_boxes.sample_tokens:
@@ -143,12 +122,9 @@ def create_tracks(all_boxes: EvalBoxes, nusc: NuScenes, eval_split: str, gt: boo
 
     # Interpolate GT and predicted tracks.
     for scene_token in tracks.keys():
-        tracks[scene_token] = interpolate_tracks(tracks[scene_token])
-
-    # Make sure the tracks are sorted in time.
-    # This is always the case for GT, but may not be the case for predictions.
-    if not gt:
-        for scene_token in tracks.keys():
+        interpolate_tracks(tracks[scene_token])
+        if not gt:
+            # Make sure predictions are sorted in in time. (Always true for GT).
             tracks[scene_token] = dict(sorted(tracks[scene_token].items(), key=lambda kv: kv[0]))
 
     return tracks
