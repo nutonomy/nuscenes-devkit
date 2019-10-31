7b17c5e5e290e7caa0c29cca006c25bbc97864c7
Holger Caesar <holger@nutonomy.com>
2019-10-31 18:38:19 +0800

    New test

--- python-sdk/nuscenes/eval/tracking/tests/test_algo.py
+++ python-sdk/nuscenes/eval/tracking/tests/test_algo.py
@@ -32,7 +32,8 @@ class TestAlgo(unittest.TestCase):
 
         return class_name, tracks_gt
 
-    def test_perfect_gt(self):
+    def test_gt_submission(self):
+        """ Test with GT submission. """
 
         # Get config.
         cfg = config_factory('tracking_nips_2019')
@@ -61,6 +62,7 @@ class TestAlgo(unittest.TestCase):
         assert np.all(md.ids == 0)
 
     def test_drop_prediction(self):
+        """ Drop one prediction from the GT submission. """
 
         # Get config.
         cfg = config_factory('tracking_nips_2019')
@@ -91,7 +93,42 @@ class TestAlgo(unittest.TestCase):
         assert md.frag[3] == 1
         assert md.ids[3] == 0
 
+    def test_drop_prediction_multiple(self):
+        """  Drop the first three predictions from the GT submission. """
+
+        # Get config.
+        cfg = config_factory('tracking_nips_2019')
+
+        # Define inputs.
+        class_name, tracks_gt = TestAlgo.single_scene()
+        verbose = False
+
+        # Remove one predicted box.
+        timestamp_boxes_pred = copy.deepcopy(tracks_gt['scene-1'])
+        timestamp_boxes_pred[0] = []
+        timestamp_boxes_pred[1] = []
+        timestamp_boxes_pred[2] = []
+        tracks_pred = {'scene-1': timestamp_boxes_pred}
+
+        # Accumulate metrics.
+        ev = TrackingEvaluation(tracks_gt, tracks_pred, class_name, cfg.dist_fcn_callable,
+                                cfg.dist_th_tp, cfg.min_recall, num_thresholds=TrackingMetricData.nelem,
+                                verbose=verbose)
+        md = ev.accumulate()
+
+        # Check outputs.
+        # Recall values above 0.75 (3/4 correct) are not achieved and therefore nan.
+        assert np.all(np.isnan(md.confidence[md.recall_hypo > 0.25]))
+        assert md.tp[8] == 1
+        assert md.fp[8] == 0
+        assert md.fn[8] == 3
+        assert md.lgd[8] == 3 * 0.5
+        assert md.tid[8] == 3 * 0.5
+        assert md.frag[8] == 0
+        assert md.ids[8] == 0
+
     def test_identity_switch(self):
+        """ Change the tracking_id of one frame from the GT submission. """
 
         # Get config.
         cfg = config_factory('tracking_nips_2019')
@@ -121,6 +158,7 @@ class TestAlgo(unittest.TestCase):
         assert md.ids[5] == 2  # One wrong id leads to 2 identity switches.
 
     def test_drop_gt(self):
+        """ Drop one box from the GT. """
 
         # Get config.
         cfg = config_factory('tracking_nips_2019')
@@ -150,6 +188,7 @@ class TestAlgo(unittest.TestCase):
         assert np.all(md.ids == 0)
 
     def test_drop_gt_interpolate(self):
+        """ Drop one box from the GT and interpolate the results to fill in that box. """
 
         # Get config.
         cfg = config_factory('tracking_nips_2019')
