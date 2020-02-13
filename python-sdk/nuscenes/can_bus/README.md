3758f7147dc7f85d07ffc42c73bab65f64c1f50c
Holger Caesar <39502217+holger-nutonomy@users.noreply.github.com>
2020-02-13 16:30:50 +0800

    nuScenes CAN bus extension (#294)
    * CAN bus expansion
    
    * Renamed folder
    
    * Improved documentation
    
    * Change folder name
    
    * Fix broken link and improved rendering
    
    * Rewording and typos
    
    * Add notes in main nuScenes readme
    
    * Improve readme
    
    * Increment devkit version

---
+++ python-sdk/nuscenes/can_bus/README.md
@@ -0,0 +1,132 @@
+# nuScenes CAN bus expansion
+This page describes the Controller Area Network (CAN) bus expansion for the nuScenes dataset.
+This is additional information that was published in January 2020 after the initial nuScenes release in March 2019.
+The data can be used for tasks such as trajectory estimation, object detection and tracking.
+
+# Overview
+- [Introduction](#introduction)
+  - [Notation](#notation)
+- [Derived messages](#derived-messages)
+  - [Meta](#meta)
+  - [Route](#route)
+- [CAN bus messages](#can-bus-messages)
+  - [IMU](#imu)
+  - [Pose](#pose)
+  - [Steer Angle Feedback](#steer-angle-feedback)
+  - [Vehicle Monitor](#vehicle-monitor)
+  - [Zoe Sensors](#zoe-sensors)
+  - [Zoe Vehicle Info](#zoe-vehicle-info)
+
+## Introduction
+The nuScenes dataset provides sensor data and annotations for 1000 scenes.
+The CAN bus expansion includes additional information for these scenes.
+The [CAN bus](https://copperhilltech.com/a-brief-introduction-to-controller-area-network/) is used for communication in automobiles and includes low-level messages regarding position, velocity, acceleration, steering, lights, battery and many more.
+In addition to this raw data we also provide some meta data, such as statistics of the different message types.
+Note that the CAN bus data is highly experimental.
+Some data may be redundant across different messages.
+Finally we extract a snippet of the route that the vehicle is currently travelling on.
+
+### Notation
+All messages of a particular type are captured in a file of the format *"scene_%04d_\*message\*.json"*.
+The messages (except *route*) contain different keys and values.
+Below we notate the dimensionality as \[d\] to indicate that a value has d dimensions.
+  
+## Derived messages
+Here we store additional information that is derived from various [CAN bus messages](#can-bus-messages) below.
+These messages are timeless and therefore do not provide the `utime` timestamp common to the CAN bus messages.
+
+### Meta
+(*scene_%04d_meta.json*)
+
+This meta file summarizes all CAN bus messages (except *route*) and provides some statistics that may be helpful to understand the data.
+- message_count: \[1\] How many messages of this type were logged.
+- message_freq: \[1\] The message frequency computed from timestamp and message_count.
+- timespan: \[1\] How many seconds passed from first to last message in a scene. Usually around 20s.
+- var_stats: (dict) Contains the maximum, mean, minimum and standard deviation for both the raw values and the differences of two consecutive values.
+
+### Route
+(*scene_%04d_route.json*)
+
+Our vehicles follow predefined routes through the city.
+The baseline route is the recommended navigation path for the vehicle to follow.
+This is an ideal route that does not take into account any blocking objects or road closures.
+The route contains the relevant section of the current scene and around 50m before and after it.
+The route is stored as a list of 2-tuples (x, y) in meters on the current nuScenes map.
+The data is recorded at approximately 50Hz.
+For 3% of the scenes this data is not available as the drivers were not following any route.
+
+## CAN bus messages
+Here we list the raw CAN bus messages.
+We store each type of message in a separate file for each scene (e.g. `scene-0001_ms_imu.json`).
+Messages are stored in chronological order in the above file. 
+Each message has the following field:
+- utime: \[1\] The integer timestamp in microseconds that the actual measurement took place (e.g. 1531883549954657).
+For the *Zoe Sensors* and *Zoe Vehicle Info* messages this info is not directly available and is therefore replaced by the timestamp when the CAN bus message was received.
+
+### IMU 
+(100Hz, *scene_%04d_ms_imu.json*)
+
+- linear_accel: \[3\] Acceleration vector (x, y, z) in the IMU frame in m/s/s.
+- q: \[4\] Quaternion that transforms from IMU coordinates to a fixed reference frame. The yaw of this reference frame is arbitrary, determined by the IMU. However, the x-y plane of the reference frame is perpendicular to gravity, and z points up. 
+- rotation_rate: \[3\] Angular velocity in rad/s around the x, y, and z axes, respectively, in the IMU coordinate frame.
+
+### Pose
+(50Hz, *scene_%04d_pose.json*)
+
+The current pose of the ego vehicle, sampled at 50Hz.
+- accel: \[3\] Acceleration vector in the ego vehicle frame in m/s/s.
+- orientation: \[4\]  The rotation vector in the ego vehicle frame.
+- pos: \[3\] The position (x, y, z) in meters in the global frame. This is identical to the [nuScenes ego pose](https://github.com/nutonomy/nuscenes-devkit/blob/master/schema.md#ego_pose), but sampled at a higher frequency.
+- rotation_rate: \[3\] The angular velocity vector of the vehicle in rad/s.  This is expressed in the ego vehicle frame.
+- vel: \[3\] The velocity in m/s, expressed in the ego vehicle frame.
+ 
+### Steer Angle Feedback
+(100Hz, *scene_%04d_steeranglefeedback.json*)
+
+- value: \[1\] Steering angle feedback in radians in range \[-7.7, 6.3\]. 0 indicates no steering, positive values indicate right turns, negative values left turns.
+
+### Vehicle Monitor
+(2Hz, *scene_%04d_vehicle_monitor.json*)
+
+- available_distance: \[1\] Available vehicle range given the current battery level in kilometers.
+- battery_level: \[1\] Current battery level in range \[0, 100\].
+- brake: \[1\] Braking pressure in bar. An integer in range \[0, 126\]. 
+- brake_switch: \[1\] Brake switch as an integer, 1 (pedal not pressed), 2 (pedal pressed) or 3 (pedal confirmed pressed).
+- gear_position: \[1\] The gear position as an integer, typically 0 (parked) or 7 (driving).
+- left_signal: \[1\] Left turning signal as an integer, 0 (inactive) or 1 (active).
+- rear_left_rpm: \[1\] Rear left brake speed in revolutions per minute.
+- rear_right_rpm: \[1\] Rear right brake speed in revolutions per minute.
+- right_signal: \[1\] Right turning signal as an integer, 0 (inactive) or 1 (active).
+- steering: \[1\] Steering angle in degrees at a resolution of 0.1 in range \[-780, 779.9\].
+- steering_speed: \[1\] Steering speed in degrees per second in range \[-465, 393\].
+- throttle: \[1\] Throttle pedal position as an integer in range \[0, 1000\].
+- vehicle_speed: \[1\] Vehicle speed in km/h at a resolution of 0.01. 
+- yaw_rate: \[1\] Yaw turning rate in degrees per second at a resolution of 0.1.
+
+### Zoe Sensors
+(794-973Hz, *scene_%04d_zoesensors.json*)
+
+- brake_sensor: \[1\] Vehicle break sensor in range \[0.375,0.411\]. High values indicate breaking.
+- steering_sensor: \[1\] Vehicle steering sensor. Same as vehicle_monitor.steering.
+- throttle_sensor: \[1\] Vehicle throttle sensor. Same as vehicle_monitor.throttle.
+
+### Zoe Vehicle Info
+(100Hz, *scene_%04d_zoe_veh_info.json*)
+
+- FL_wheel_speed: \[1\] Front left wheel speed. The unit is rounds per minute with a resolution of 0.0417rpm.
+- FR_wheel_speed: \[1\] Front right wheel speed. The unit is rounds per minute with a resolution of 0.0417rpm.
+- RL_wheel_speed: \[1\] Rear left wheel speed. The unit is rounds per minute with a resolution of 0.0417rpm.
+- RR_wheel_speed: \[1\] Rear right wheel speed. The unit is rounds per minute with a resolution of 0.0417rpm.
+- left_solar: \[1\] Zoe vehicle left solar sensor value as an integer.
+- longitudinal_accel: \[1\] Longitudinal acceleration in meters per second squared at a resolution of 0.05.
+- meanEffTorque: \[1\] Actual torque delivered by the engine in Newton meters at a resolution of 0.5. Values in range \[-400, 1647\], offset by -400.
+- odom: \[1\] Odometry distance travelled modulo vehicle circumference. Values are in centimeters in range \[0, 124\]. Note that due to the low sampling frequency these values are only useful at low speeds.
+- odom_speed: \[1\] Vehicle speed in km/h. Values in range \[0, 60\]. For a higher sampling rate refer to the pose.vel message.
+- pedal_cc: \[1\] Throttle value. Values in range \[0, 1000\].
+- regen: \[1\] Coasting throttle. Values in range \[0, 100\].
+- requestedTorqueAfterProc: \[1\] Input torque requested in Newton meters at a resolution of 0.5. Values in range \[-400, 1647\], offset by -400.
+- right_solar: \[1\] Zoe vehicle right solar sensor value as an integer.
+- steer_corrected: \[1\] Steering angle (steer_raw) corrected by an offset (steer_offset_can).
+- steer_offset_can: \[1\] Steering angle offset in degrees, typically -12.6.
+- steer_raw: \[1\] Raw steering angle in degrees.
+- transversal_accel: \[1\] Transversal acceleration in g at a resolution of 0.004.
---
+++ python-sdk/nuscenes/can_bus/can_bus_api.py
@@ -0,0 +1,253 @@
+# nuScenes dev-kit.
+# Code written by Holger Caesar, 2020.
+
+import argparse
+import json
+import os
+import re
+import warnings
+from typing import Dict, List, Tuple, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.spatial.distance as scipy_dist
+
+
+class NuScenesCanBus:
+    """
+    This class encapsulates the files of the nuScenes CAN bus expansion set.
+    It can be used to access the baseline navigation route as well as the various CAN bus messages.
+    """
+
+    def __init__(self,
+                 dataroot: str = '/data/sets/nuscenes',
+                 max_misalignment: float = 5.0):
+        """
+        Initialize the nuScenes CAN bus API.
+        :param dataroot: The nuScenes directory where the "can" folder is located.
+        :param max_misalignment: Maximum distance in m that any pose is allowed to be away from the route.
+        """
+        # Check that folder exists.
+        self.can_dir = os.path.join(dataroot, 'can_bus')
+        if not os.path.isdir(self.can_dir):
+            raise Exception('Error: CAN bus directory not found: %s. Please download it from '
+                            'https://www.nuscenes.org/download' % self.can_dir)
+
+        # Define blacklist for scenes where route and ego pose are not aligned.
+        if max_misalignment == 5.0:
+            # Default settings are hard-coded for performance reasons.
+            self.route_blacklist = [
+                71, 73, 74, 75, 76, 85, 100, 101, 106, 107, 108, 109, 110, 112, 113, 114, 115, 116, 117, 118, 119,
+                261, 262, 263, 264, 276, 302, 303, 304, 305, 306, 334, 388, 389, 390, 436, 499, 500, 501, 502, 504,
+                505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 517, 518, 547, 548, 549, 550, 551, 556, 557,
+                558, 559, 560, 561, 562, 563, 564, 565, 730, 731, 733, 734, 735, 736, 737, 738, 778, 780, 781, 782,
+                783, 784, 904, 905, 1073, 1074
+            ]
+        else:
+            misaligned = self.list_misaligned_routes()
+            self.route_blacklist = [int(s[-4:]) for s in misaligned]
+
+        # Define blacklist for scenes without CAN bus data.
+        self.can_blacklist = [
+            161, 162, 163, 164, 165, 166, 167, 168, 170, 171, 172, 173, 174, 175, 176, 309, 310, 311, 312, 313, 314
+        ]
+
+        # Define all messages.
+        self.can_messages = [
+            'ms_imu', 'pose', 'steeranglefeedback', 'vehicle_monitor', 'zoesensors', 'zoe_veh_info'
+        ]
+        self.derived_messages = [
+            'meta', 'route'
+        ]
+        self.all_messages = self.can_messages + self.derived_messages
+
+    def print_can_message_stats(self,
+                                scene_name: str) -> None:
+        """
+        Prints the meta stats for each CAN message type of a particular scene.
+        :param scene_name: The name of the scene, e.g. scene-0001.
+        """
+        for message_name in self.can_messages:
+            self.print_message_stats(scene_name, message_name)
+
+    def print_message_stats(self,
+                            scene_name: str,
+                            message_name: str) -> None:
+        """
+        Prints the meta stats for a particular scene and message name.
+        :param scene_name: The name of the scene, e.g. scene-0001.
+        :param message_name: The name of the CAN bus message type, e.g. ms_imu.
+        """
+        assert message_name != 'meta', 'Error: Cannot print stats for meta '
+        messages = self.get_messages(scene_name, 'meta')
+        print(json.dumps(messages[message_name], indent=2))
+
+    def plot_baseline_route(self,
+                            scene_name: str,
+                            out_path: str = None) -> None:
+        """
+        Plot the baseline route and the closest ego poses for a scene.
+        Note that the plot is not closed and should be closed by the caller.
+        :param scene_name: The name of the scene, e.g. scene-0001.
+        :param out_path: Output path to dump the plot to. Ignored if None.
+        """
+        # Get data.
+        route, pose = self.get_pose_and_route(scene_name)
+
+        # Visualize.
+        plt.figure()
+        plt.plot(route[:, 0], route[:, 1])
+        plt.plot(pose[:, 0], pose[:, 1])
+        plt.plot(pose[0, 0], pose[0, 1], 'rx', MarkerSize=10)
+        plt.legend(('Route', 'Pose', 'Start'))
+        plt.xlabel('Map coordinate x in m')
+        plt.ylabel('Map coordinate y in m')
+        if out_path is not None:
+            plt.savefig(out_path)
+        plt.show()
+
+    def plot_message_data(self,
+                          scene_name: str,
+                          message_name: str,
+                          key_name: str,
+                          dimension: int = 0,
+                          out_path: str = None,
+                          plot_format: str = 'b-') -> None:
+        """
+        Plot the data for a particular message.
+        :param scene_name: The name of the scene, e.g. scene-0001.
+        :param message_name: The name of the CAN bus message type, e.g. ms_imu.
+        :param key_name: The name of the key in the message, e.g. linear_accel.
+        :param dimension: Which dimension to render (default is 0). If -1, we render the norm of the values.
+        :param out_path: Output path to dump the plot to. Ignored if None.
+        :param plot_format: A string that describes a matplotlib format, by default 'b-' for a blue dashed line.
+        """
+        # Get data.
+        messages = self.get_messages(scene_name, message_name)
+        data = np.array([m[key_name] for m in messages])
+        utimes = np.array([m['utime'] for m in messages])
+
+        # Convert utimes to seconds and subtract the minimum.
+        utimes = (utimes - min(utimes)) / 1e6
+
+        # Take selected column.
+        if dimension == -1:
+            data = np.linalg.norm(data, axis=1)
+        elif dimension == 0:
+            pass
+        elif data.ndim > 1 and data.shape[1] >= dimension + 1:
+            data = data[:, dimension]
+        else:
+            raise Exception('Error: Invalid dimension %d for key "%s"!' % (dimension, key_name))
+
+        # Render.
+        plt.figure()
+        plt.plot(utimes, data, plot_format, markersize=1)
+        plt.title(scene_name)
+        plt.xlabel('Scene time in s')
+        plt.ylabel('%s - %s' % (message_name, key_name))
+        if out_path is not None:
+            plt.savefig(out_path)
+        plt.show()
+
+    def list_misaligned_routes(self,
+                               max_misalignment: float = 5.0) -> List[str]:
+        """
+        Print all scenes where ego poses and baseline route are misaligned.
+        We use the Hausdorff distance to decide on the misalignment.
+        :param max_misalignment: Maximum distance in m that any pose is allowed to be away from the route.
+        :return: A list of all the names of misaligned scenes.
+        """
+        # Get all scenes.
+        all_files = os.listdir(self.can_dir)
+        scene_list = list(np.unique([f[:10] for f in all_files]))  # Get the scene name from e.g. scene-0123_meta.json.
+
+        # Init.
+        misaligned = []
+
+        for scene_name in scene_list:
+            # Get data.
+            route, pose = self.get_pose_and_route(scene_name, print_warnings=False)
+
+            # Filter by Hausdorff distance.
+            dists = scipy_dist.cdist(pose, route)
+            max_dist = np.max(np.min(dists, axis=1))
+            if max_dist > max_misalignment:
+                misaligned.append(scene_name)
+
+        return misaligned
+
+    def get_pose_and_route(self,
+                           scene_name: str,
+                           print_warnings: bool = True) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Return the route and pose for a scene as numpy arrays.
+        :param scene_name: The name of the scene, e.g. scene-0001.
+        :param print_warnings: Whether to print out warnings if the requested data is not available or not reliable.
+        :return: A tuple of route and pose arrays (each point is 2d).
+        """
+        # Load baseline route and poses.
+        route = self.get_messages(scene_name, 'route', print_warnings=print_warnings)
+        pose = self.get_messages(scene_name, 'pose', print_warnings=print_warnings)
+
+        # Convert to numpy format.
+        route = np.asarray(route)
+        pose = np.asarray([p['pos'][:2] for p in pose])
+
+        return route, pose
+
+    def get_messages(self,
+                     scene_name: str,
+                     message_name: str,
+                     print_warnings: bool = True) -> Union[List[Dict], Dict]:
+        """
+        Retrieve the messages for a particular scene and message type.
+        :param scene_name: The name of the scene, e.g. scene-0001.
+        :param message_name: The name of the CAN bus message type, e.g. ms_imu.
+        :param print_warnings: Whether to print out warnings if the requested data is not available or not reliable.
+        :return: The raw contents of the message type, either a dict (for `meta`) or a list of messages.
+        """
+        # Check inputs. Scene names must be in the format scene-0123.
+        assert re.match('^scene-\\d\\d\\d\\d$', scene_name)
+        assert message_name in self.all_messages, 'Error: Invalid CAN bus message name: %s' % message_name
+
+        # Check for data issues.
+        scene_id = int(scene_name[-4:])
+        if scene_id in self.can_blacklist:
+            # Check for logs that have no CAN bus data.
+            raise Exception('Error: %s does not have any CAN bus data!' % scene_name)
+        elif print_warnings:
+            # Print warnings for scenes that are known to have bad data.
+            if message_name == 'route':
+                if scene_id in self.route_blacklist:
+                    warnings.warn('Warning: %s is not well aligned with the baseline route!' % scene_name)
+            elif message_name == 'vehicle_monitor':
+                if scene_id in [419]:
+                    warnings.warn('Warning: %s does not have any vehicle_monitor messages!')
+
+        # Load messages.
+        message_path = os.path.join(self.can_dir, '%s_%s.json' % (scene_name, message_name))
+        with open(message_path, 'r') as f:
+            messages = json.load(f)
+        assert type(messages) in [list, dict]
+
+        # Rename all dict keys to lower-case.
+        if isinstance(messages, dict):
+            messages = {k.lower(): v for (k, v) in messages.items()}
+
+        return messages
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plot stats for the CAN bus API.')
+    parser.add_argument('--dataroot', type=str, default='/data/sets/nuscenes')
+    parser.add_argument('--scene_name', type=str, default='scene-0028')
+    parser.add_argument('--message_name', type=str, default='steeranglefeedback')
+    parser.add_argument('--key_name', type=str, default='value')
+    args = parser.parse_args()
+
+    nusc_can = NuScenesCanBus(dataroot=args.dataroot)
+    if args.message_name == 'route+pose':
+        nusc_can.plot_baseline_route(args.scene_name)
+    else:
+        nusc_can.plot_message_data(args.scene_name, args.message_name, args.key_name)
