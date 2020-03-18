e5daa3cae047453cb88fb2ac91bd6b4cb8db7221
Holger Caesar <holger@nutonomy.com>
2020-03-18 10:29:01 +0800

    Add other notebooks for testing

--- setup/test_tutorial.sh
+++ setup/test_tutorial.sh
@@ -5,11 +5,15 @@ set -ex
 source activate nuscenes
 
 # Generate python script from Jupyter notebook and then copy into Docker image.
-jupyter nbconvert --to python python-sdk/tutorials/nuscenes_basics_tutorial.ipynb || { echo "Failed to convert notebook to python script"; exit 1; }
+jupyter nbconvert --to python python-sdk/tutorials/nuscenes_basics_tutorial.ipynb || { echo "Failed to convert nuscenes_basics_tutorial notebook to python script"; exit 1; }
+jupyter nbconvert --to python python-sdk/tutorials/can_bus_tutorial.ipynb || { echo "Failed to convert can_bus_tutorial notebook to python script"; exit 1; }
+jupyter nbconvert --to python python-sdk/tutorials/map_expansion_tutorial.ipynb || { echo "Failed to convert map_expansion_tutorial notebook to python script"; exit 1; }
 
 # Remove extraneous matplot inline command and comment out any render* methods.
 sed -i.bak "/get_ipython.*/d; s/\(nusc\.render.*\)/#\1/" python-sdk/tutorials/nuscenes_basics_tutorial.py || { echo "error in sed command"; exit 1; }
 
 # Run tutorial
 xvfb-run python python-sdk/tutorials/nuscenes_basics_tutorial.py
+xvfb-run python python-sdk/tutorials/can_bus_tutorial.py
+xvfb-run python python-sdk/tutorials/map_expansion_tutorial.py
 
