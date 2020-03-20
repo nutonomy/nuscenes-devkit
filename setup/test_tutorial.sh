941669ade688c03facfb1820058cb89414dcafec
freddy <freddy.boulton@nutonomy.com>
2020-03-20 11:13:24 -0400

    Adding prediction tutorial to tutorial directory

--- setup/test_tutorial.sh
+++ setup/test_tutorial.sh
@@ -8,6 +8,7 @@ source activate nuscenes
 jupyter nbconvert --to python python-sdk/tutorials/nuscenes_basics_tutorial.ipynb || { echo "Failed to convert nuscenes_basics_tutorial notebook to python script"; exit 1; }
 jupyter nbconvert --to python python-sdk/tutorials/can_bus_tutorial.ipynb || { echo "Failed to convert can_bus_tutorial notebook to python script"; exit 1; }
 jupyter nbconvert --to python python-sdk/tutorials/map_expansion_tutorial.ipynb || { echo "Failed to convert map_expansion_tutorial notebook to python script"; exit 1; }
+jupyter nbconvert --to python python-sdk/tutorials/prediction_tutorial.ipynb || { echo "Failed to convert prediction notebook to python script"; exit 1; }
 
 # Remove extraneous matplot inline command and comment out any render* methods.
 sed -i.bak "/get_ipython.*/d; s/\(nusc\.render.*\)/#\1/" python-sdk/tutorials/nuscenes_basics_tutorial.py || { echo "error in sed command"; exit 1; }
@@ -16,4 +17,4 @@ sed -i.bak "/get_ipython.*/d; s/\(nusc\.render.*\)/#\1/" python-sdk/tutorials/nu
 xvfb-run python python-sdk/tutorials/nuscenes_basics_tutorial.py
 xvfb-run python python-sdk/tutorials/can_bus_tutorial.py
 xvfb-run python python-sdk/tutorials/map_expansion_tutorial.py
-
+xvfb-run python python-sdk/tutorials/prediction_tutorial.py
