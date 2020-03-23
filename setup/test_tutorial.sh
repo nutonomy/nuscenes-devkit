652d9280affe11721be8411a3b12d78838f7eea0
Holger Caesar <holger@nutonomy.com>
2020-03-23 15:00:20 +0800

    Disable rendering

--- setup/test_tutorial.sh
+++ setup/test_tutorial.sh
@@ -12,6 +12,9 @@ jupyter nbconvert --to python python-sdk/tutorials/prediction_tutorial.ipynb || 
 
 # Remove extraneous matplot inline command and comment out any render* methods.
 sed -i.bak "/get_ipython.*/d; s/\(nusc\.render.*\)/#\1/" python-sdk/tutorials/nuscenes_basics_tutorial.py || { echo "error in sed command"; exit 1; }
+sed -i.bak "/get_ipython.*/d; s/\(nusc_can.plot_baseline_route.*\)/#\1/"  python-sdk/tutorials/can_bus_tutorial.py || { echo "error in sed command"; exit 1; }
+sed -i.bak "/get_ipython.*/d; s/\(^plt.*\)/#\1/"  python-sdk/tutorials/can_bus_tutorial.py || { echo "error in sed command"; exit 1; }
+sed -i.bak "/get_ipython.*/d; s/\(fig, ax.*\)/#\1/"  python-sdk/tutorials/map_expansion_tutorial.py || { echo "error in sed command"; exit 1; }
 sed -i.bak "/get_ipython.*/d; s/\(plt.imshow.*\)/#\1/"  python-sdk/tutorials/prediction_tutorial.py || { echo "error in sed command"; exit 1; }
 
 # Run tutorial
