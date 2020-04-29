9f375858b75ec98845b3d7e5b8ce91ff9d98edfc
Holger Caesar <holger@nutonomy.com>
2020-04-29 17:46:25 +0800

    Explicitly init tables

--- python-sdk/nuimages/nuimages.py
+++ python-sdk/nuimages/nuimages.py
@@ -32,7 +32,7 @@ class NuImages:
                  verbose: bool = True):
         """
         Loads database and creates reverse indexes and shortcuts.
-        :param version: Version to load (e.g. "v1.0", ...).
+        :param version: Version to load (e.g. "v0.1", ...).
         :param dataroot: Path to the tables and data.
         :param lazy: Whether to use lazy loading for the database tables.
         :param verbose: Whether to print status messages during load.
@@ -49,23 +49,26 @@ class NuImages:
         if verbose:
             print("======\nLoading NuScenes tables for version {}...".format(self.version))
 
-        # Explicitly assign tables to help the IDE determine valid class members.
-        if not lazy:
-            for table in self.table_names:
-                self.__setattr__(table, self.__load_table__(table))
+        # Explicitly init tables to help the IDE determine valid class members.
+        self.attribute = None
+        self.camera = None
+        self.category = None
+        self.image = None
+        self.log = None
+        self.object_ann = None
+        self.surface_ann = None
 
-            # self.attribute = self.__load_table__('attribute')  # Verify that these are still needed
-            # self.camera = self.__load_table__('camera')
-            # self.category = self.__load_table__('category')
-            # self.image = self.__load_table__('image')
-            # self.log = self.__load_table__('log')
-            # self.object_ann = self.__load_table__('object_ann')
-            # self.surface_ann = self.__load_table__('surface_ann')
+        # Init reverse indexing.
         self._token2ind: Dict[str, dict] = dict()
         for table in self.table_names:
             self._token2ind[table] = None
 
-        # Initialize NuImagesExplorer class
+        # Load tables directly if requested.
+        if not lazy:
+            for table in self.table_names:
+                self.__setattr__(table, self.__load_table__(table))
+
+        # Initialize NuImagesExplorer class.
         self.explorer = NuImagesExplorer(self)
 
     def __getattr__(self, attr_name: str) -> Any:
