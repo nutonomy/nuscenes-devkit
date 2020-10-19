05cd0f6043878331b0785945e8ea515d670cb833
whyekit-aptiv <whyekit.fong@nutonomy.com>
2020-10-19 14:19:00 +0800

    Add methods to ensure labels are converted correctly

--- python-sdk/nuscenes/eval/lidarseg/utils.py
+++ python-sdk/nuscenes/eval/lidarseg/utils.py
@@ -5,15 +5,14 @@ from nuscenes import NuScenes
 
 class LidarsegChallengeAdaptor:
     def __init__(self, nusc: NuScenes):
-        self.rawname2mergedname_mapping = self.get_raw2merged()
-        self.mergedname2mergedidx_mapping = self.get_merged2idx()
+        self.nusc = nusc
+
+        self.raw_name_2_merged_name_mapping = self.get_raw2merged()
+        self.merged_name_2_merged_idx_mapping = self.get_merged2idx()
 
         self.check_mapping()
 
-        self.rawidx2mergedidx_mapping = dict()
-        for rawname, rawidx in nusc.lidarseg_name2idx_mapping.items():
-            self.rawidx2mergedidx_mapping[rawidx] = self.mergedname2mergedidx_mapping[
-                self.rawname2mergedname_mapping[rawname]]
+        self.raw_idx_2_merged_idx_mapping = self.get_raw_idx_2_merged_idx()
 
     @staticmethod
     def get_raw2merged():
@@ -72,32 +71,77 @@ class LidarsegChallengeAdaptor:
                 'manmade': 15,
                 'vegetation': 16}
 
+    def get_raw_idx_2_merged_idx(self):
+        raw_idx_2_merged_idx_mapping = dict()
+        for raw_name, raw_idx in self.nusc.lidarseg_name2idx_mapping.items():
+            raw_idx_2_merged_idx_mapping[raw_idx] = self.merged_name_2_merged_idx_mapping[
+                self.raw_name_2_merged_name_mapping[raw_name]]
+        return raw_idx_2_merged_idx_mapping
+
     def check_mapping(self):
         merged_set = set()
-        for raw_name, merged_name in self.rawname2mergedname_mapping.items():
+        for raw_name, merged_name in self.raw_name_2_merged_name_mapping.items():
             merged_set.add(merged_name)
 
-        assert len(merged_set) == len(self.mergedname2mergedidx_mapping), 'Error: Number of merged classes is not ' \
-                                                                          'the same as the number of merged indices.'
+        assert len(merged_set) == len(self.merged_name_2_merged_idx_mapping), \
+            'Error: Number of merged classes is not the same as the number of merged indices.'
 
     def convert_label(self, points_label: np.ndarray) -> np.ndarray:
         """
         Convert the labels in a single .bin file according to the provided mapping
         :param points_label: The .bin to be converted (e.g. './i_contain_the_labels_for_a_pointcloud.bin')
         """
-
-        # counter_before = get_stats(points_label)  # get stats before conversion
+        counter_before = self.get_stats(points_label)  # get stats before conversion
 
         # Map the labels accordingly; if there are labels present in points_label but not in the map,
         # an error will be thrown
-        points_label = np.vectorize(self.rawidx2mergedidx_mapping.__getitem__)(points_label)
+        points_label = np.vectorize(self.raw_idx_2_merged_idx_mapping.__getitem__)(points_label)
 
-        # counter_after = get_stats(points_label)  # get stats after conversion
-        """
-        assert compare_stats(counter_before, counter_after), 'Error: Statistics of labels for {} have changed ' \
-                                                             'after conversion. Pls check.'.format(bin_file_in)
-        """
+        counter_after = self.get_stats(points_label)  # get stats after conversion
+
+        assert self.compare_stats(counter_before, counter_after), 'Error: Statistics of labels have changed ' \
+                                                                  'after conversion. Pls check.'
+
         return points_label
+
+    def compare_stats(self, counter_before: np.array, counter_after: np.array) -> bool:
+        """
+        Compare stats for a single .bin file before and after conversion.
+        :param counter_before: A numPy array which contains the counts of each class (the index of the array corresponds
+                               to the class label), before conversion; e.g. np.array([0, 1, 34, ...]) --> class 0 has
+                               no points, class 1 has 1 point, class 2 has 34 points, etc.
+        :param counter_after: A numPy array which contains the counts of each class (the index of the array corresponds
+                              to the class label) after conversion
+        :returns: True or False; True if the stats before and after conversion are the same, and False if otherwise.
+        """
+        counter_check = [0] * len(counter_after)
+        for i, count in enumerate(counter_before):  # Note that it is expected that the class labels are 0-indexed.
+            counter_check[self.raw_idx_2_merged_idx_mapping[i]] += count
+
+        comparison = counter_check == counter_after
+
+        return comparison
+
+    def get_stats(self, points_label: np.array) -> np.ndarray:
+        """
+        Get frequency of each label in a point cloud.
+        :param points_label: A numPy array which contains the labels of the point cloud; e.g. np.array([2, 1, 34, ..., 38])
+        :returns: An array which contains the counts of each label in the point cloud. The index of the point cloud
+                  corresponds to the index of the class label. E.g. [0, 2345, 12, 451] means that there are no points
+                  in class 0, there are 2345 points in class 1, there are 12 points in class 2 etc.
+        """
+        # Create "buckets" to store the counts for each label; the number of "buckets" is the larger of the number
+        # of classes in nuScenes-lidarseg and lidarseg challenge.
+        lidarseg_counts = [0] * (max(max(self.raw_idx_2_merged_idx_mapping.keys()),
+                                     max(self.raw_idx_2_merged_idx_mapping.values())) + 1)
+
+        indices: np.ndarray = np.bincount(points_label)
+        ii = np.nonzero(indices)[0]
+
+        for class_idx, class_count in zip(ii, indices[ii]):
+            lidarseg_counts[class_idx] += class_count  # Increment the count for the particular class name.
+
+        return lidarseg_counts
 
 
 if __name__ == '__main__':
