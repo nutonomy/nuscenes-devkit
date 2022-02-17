6f8043a2558674b689e1c4b64d48c55487a5aa12
whyekit-aptiv <whyekit.fong@nutonomy.com>
2022-02-17 11:04:39 +0800

    Fix bug when padding values with nans for unachieved recall thresholds

--- python-sdk/nuscenes/eval/tracking/algo.py
+++ python-sdk/nuscenes/eval/tracking/algo.py
@@ -200,8 +200,8 @@ class TrackingEvaluation(object):
                 values = np.concatenate([([v] * r) for (v, r) in zip(values, rep_counts)])
 
                 # Pad values with nans for unachieved recall thresholds.
-                all_values = [np.nan] * unachieved_thresholds
-                all_values = np.append(all_values, values)
+                all_values = [np.nan] * num_unachieved_thresholds
+                all_values.extend(values)
 
             assert len(all_values) == TrackingMetricData.nelem
             md.set_metric(metric_name, all_values)
