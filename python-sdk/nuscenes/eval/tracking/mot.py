ebf4072e90b279c91d33282106f803c2677f9538
MarcSo <35328154+MarcSo@users.noreply.github.com>
2023-04-17 15:58:40 +0200

    Update mot.py
    Fixed pandas call

--- python-sdk/nuscenes/eval/tracking/mot.py
+++ python-sdk/nuscenes/eval/tracking/mot.py
@@ -122,7 +122,7 @@ class MOTAccumulatorCustom(motmetrics.mot.MOTAccumulator):
                 copy['HId'] = copy['HId'].map(lambda x: hid_map[x], na_action='ignore')
                 infos['hid_map'] = hid_map
 
-            r = pd.concat(r, copy)
+            r = pd.concat((r, copy))
             mapping_infos.append(infos)
 
         if return_mappings:
